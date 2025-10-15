569ec6316717e81aeae78358f2e3a60081685726
mironshoh <mironshohasadov1603@gmail.com>
2025-10-15 21:14:44 +0500

    update db and logs

--- src/main.ts
+++ src/main.ts
@@ -2,32 +2,12 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
 import { NestExpressApplication } from "@nestjs/platform-express";
 import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
-import * as path from "path";
 
-import { join } from "path";
-import { existsSync } from "fs";
-
-const ffprobeLocal = join(process.cwd(), "ffprobe.exe");
-
-if (existsSync(ffprobeLocal)) {
-  process.env.FFPROBE_PATH = ffprobeLocal;
-  try {
-    const ffmpeg = require("fluent-ffmpeg");
-    ffmpeg.setFfprobePath(ffprobeLocal);
-    console.log("ffprobe yo'li o'rnatildi:", ffprobeLocal);
-  } catch {}
-} else {
-  console.warn("ffprobe.exe topilmadi!");
-}
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
   app.enableCors(); // CORS yoqish
-  console.log(
-    "Fayllar serve qilinayotgan joy: ",
-    path.resolve(process.cwd(), "uploads")
-  );
 
   // ✅ Swagger sozlamalari
   const config = new DocumentBuilder()
