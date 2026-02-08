780caf979464edf4a2083d4782999621231a7285
Mironshoh <mironshohasadov1603@gmail.com>
2026-02-08 20:42:29 +0500

    acc cors logics

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,23 @@ import { DocumentBuilder, SwaggerModule } from "@nestjs/swagger";
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  app.enableCors(); // CORS yoqish
+  app.enableCors({
+    origin: [
+      "https://doc.mironshokh.uz",
+      "http://localhost:3000",
+      "http://localhost:5173",
+      "http://localhost:3001",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    credentials: true,
+  });
 
   // ✅ Swagger sozlamalari
   const config = new DocumentBuilder()
     .setTitle("Autotest API")
     .setDescription("Autotest API")
     .setVersion("1.0")
-    .addServer("http://api.mironshokh.uz") // Lokal server qo'shish
+    .addServer("https://api.mironshokh.uz") // Lokal server qo'shish
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("api/docs", app, document);
